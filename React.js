d1d8adfc8713925132ca0e16b0356757a438a618
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-23 21:16:39 +0530

    Update React.js

--- React.js
+++ React.js
@@ -1,1 +1,6 @@
-//What is ReactJs
+//What is ReactJs - Reactjs is java scripts library for Bulding User interface 
+==========
+  ReactJs client side java scripts library, its
+  All about Bulding Modarn, rective user interface for web site 
+  
+  
