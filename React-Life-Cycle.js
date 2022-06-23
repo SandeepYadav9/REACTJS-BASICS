1027a17b14116f177c88d18e77172aae88fa6218
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-24 01:58:56 +0530

    Create React-Life-Cycle.js

---
+++ React-Life-Cycle.js
@@ -0,0 +1,12 @@
+
+  REACT LIFE CYCLE - 3 Phase 
+  
+  1 Mount  2. Update   3. UnMount
+  
+  1. Mount 
+  
+  - componentWillMount (){}
+    - Immediately Before initial rendring 
+    
+  - componentDidMount (){}
+   - Immedate af
