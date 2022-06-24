1514354c2d5a0b5d823dfe62b1a06c7c8ef481d2
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-24 11:32:38 +0530

    Update React-Life-Cycle.js

--- React-Life-Cycle.js
+++ React-Life-Cycle.js
@@ -9,4 +9,23 @@
     - Immediately Before initial rendring 
     
   - componentDidMount (){}
-   - Immedate af
+   - Immedate after initialize rendring 
+   
+   - componentWillReciveProps (nextProp){}
+     - Will component recive new Props
+     
+    2. Update
+    
+     shouldComponentUpdate(nextProp, nextState){}
+     - Before rendring recive newPprop or newState 
+     
+     componentWillUpdate(nextProps, nextState){}
+     - Before rendring after reciving new props or state
+     
+     componentDidUpdate(prevProps, prevState){}
+     - After component update are flushed to dom 
+     
+     3. UnMounting
+     
+     componentWillUnMount(){}
+     - Before removing components from DOM
