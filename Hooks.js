67b6dd7fb56030cdcc98f28072dba5b01d175279
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-25 00:32:06 +0530

    Update Hooks.js

--- Hooks.js
+++ Hooks.js
@@ -1,11 +1,16 @@
 Hooks: Hooks come to the react  16.8 versions 
 
-some hooks are
+some hooks are hear 
 
 1. useState();
 2. useReducer();
 3. useReff();
+5. useContext();
+
 4. useEffect();
 
-5. useContext();
+
+6. useMemo(); 
+7. useCallback();
+8. React.memo()
 
