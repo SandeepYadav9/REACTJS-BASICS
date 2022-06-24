329a6d8db94f41ff6d0ea53e1b4bcd287c0e8629
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-24 17:44:39 +0530

    Create Control-UnControl-Components.js

---
+++ Control-UnControl-Components.js
@@ -0,0 +1,8 @@
+UnControl-Components - input components another name is UnControl Components, 
+                       Because input have some internal state , can not controled by React( React used own state  ).
+                       
+                   Note -1. input and form have default behaver take input via user  
+                         2. input and form have own internal state 
+                       ==================================
+
+2. Control-Components - components that controled by React called Control Components
