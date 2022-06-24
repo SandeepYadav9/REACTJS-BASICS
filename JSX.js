a3ce4435afacf4764b6763e41f2aa1dfa6d63e7a
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-24 13:56:19 +0530

    Create JSX.js

---
+++ JSX.js
@@ -0,0 +1,16 @@
+JSX - JSX stands for JAVA SCRIPTS XML 
+    - MEANs inside java scripts used HTML code 
+    
+    Ex.
+    // in java scripts function (ES-6) used html code (div, h1, span)
+    const funName=()=>{ 
+    return ( 
+    <div> 
+      <h1>Sandeep</h1>
+     <span>Yadav</span>
+      </div>
+    )
+    }
+    exports default funName;
+    
+    
