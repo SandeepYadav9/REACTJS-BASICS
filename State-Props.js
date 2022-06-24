169cd4092040ab6ef0590840912e976171c15de5
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-24 13:46:00 +0530

    Create State-Props.js

---
+++ State-Props.js
@@ -0,0 +1,4 @@
+STATE- State are inbuild object in ReactJs that contain some information  of your component if state change your component rerender 
+       - State are mutable  
+  
+PROPS- Props only comunicate information between Parent component to child components (imutable )
