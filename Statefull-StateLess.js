3c34aae3620ac04c744b9c3d8cb967b13918d04c
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-24 14:29:27 +0530

    Create Statefull-StateLess.js

---
+++ Statefull-StateLess.js
@@ -0,0 +1,22 @@
+Statefull - Classbased components are statefull because its contain some data and update them via render method 
+
+class App extends React.Components {
+render{
+return (
+     <div>
+       <h1>I am thinking Step by step </h1>
+     </div>
+      )
+    }
+                                   
+extands default App;
+
+===================================
+StateLess Components -  function Based componnets are Stetless Because its not contain state or method
+
+const App=()=>{
+  return (
+    <div> Hi</div>
+  )
+}
+exports default App;
