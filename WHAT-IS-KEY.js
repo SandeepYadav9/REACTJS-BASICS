ec70a00664ed9b9ce506afe962c625eab6c607b2
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-25 22:17:28 +0530

    Update WHAT-IS-KEY.js

--- WHAT-IS-KEY.js
+++ WHAT-IS-KEY.js
@@ -2,6 +2,6 @@
 
 
 KEY- key is unique identifire and it is identify which item have changed, 
-     been updated or deleted fro the list 
+     been updated or deleted from the list 
      
   - Which components are going to render in React
