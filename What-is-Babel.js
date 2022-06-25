441e5c21e5e588d50a4e18f6719b0dae87ecced7
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-25 23:37:58 +0530

    Update What-is-Babel.js

--- What-is-Babel.js
+++ What-is-Babel.js
@@ -1,1 +1,9 @@
-Babel -
+Babel -  Browser can not read Modarn Java Scripts (JSX) directoly , 
+  
+  Browser read only regular javascripts object 
+  
+  for read web Browser JSX file , the file needs to be a transformed into regular
+  java scripts object.
+  
+  
+
