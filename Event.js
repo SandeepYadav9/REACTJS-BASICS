2a80f530f1908b35928da3a1427c37b8cdffef41
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-26 02:58:19 +0530

    Update Event.js

--- Event.js
+++ Event.js
@@ -1,4 +1,6 @@
 What is Event- Event is an action that user triger, such as passsing a key, 
   onClickHandler
 
+eventpriventDefault - is your synthetic event.
+
 <button onClick={onClickHandler}>TrigerHandler</button>
