ba42606869193684d2b98b98566aa7e57397b6a2
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-06-25 22:17:03 +0530

    Update and rename WhatisKey.js to WHAT-IS-KEY.js

---
+++ WHAT-IS-KEY.js
@@ -0,0 +1,7 @@
+
+
+
+KEY- key is unique identifire and it is identify which item have changed, 
+     been updated or deleted fro the list 
+     
+  - Which components are going to render in React
