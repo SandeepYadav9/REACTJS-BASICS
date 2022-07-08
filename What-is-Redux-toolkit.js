5b0c81f0a11c9d15dc304c4d74b24876e4ac8be1
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-07-08 07:37:30 +0530

    Update What-is-Redux-toolkit.js

--- What-is-Redux-toolkit.js
+++ What-is-Redux-toolkit.js
@@ -1,11 +1,13 @@
-Redux-  Redux is javaScripts library for managing centralizing application state
+/* Redux-  Redux is javaScripts library for managing centralizing application state
         .it is most commonly used with libraries such ass React cLIENT-SIDE APPLICATION
          Interface.
          
          Redux vs Context-api - 
            
-           For high friquency We can use Redux
-           Low Friquency i can use CONTEXT-API
+           For high friquency cahnge State We can use Redux
+           Low Friquency change state i can use CONTEXT-API
+           
+           /*
 
 
          
