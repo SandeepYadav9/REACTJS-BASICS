4e77c0d44c8367cd1a7d747b513eabe292d82fb9
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-07-09 21:00:22 +0530

    Update What-is-Redux-toolkit.js

--- What-is-Redux-toolkit.js
+++ What-is-Redux-toolkit.js
@@ -7,6 +7,39 @@
            For high friquency cahnge State We can use Redux
            Low Friquency change state i can use CONTEXT-API
            
+           
+           1. install Package 
+           yarn add 
+           @reduxjs/toolkit
+           react-redux
+           
+           2. create Redux store 
+              src/store
+              src/store/index.js
+              
+              in index folder import configureStore API form Redux tookits.
+              
+              ex.              
+             import {configureStore} from '@reduxjs/toolkit';
+             const store = configureStore({
+             reducer:{
+             
+                   },
+             
+             })
+             export default store;
+             
+            
+           3.  tell react i am going to use Redux Store 
+             via <Provider> around our application 
+             src/index.js 
+             -import Redux store we just created , put a 
+             a <Provider> around your <App> 
+             
+           4 . Make a Redux State Slice 
+           5. Use  Action Creators 
+                 
+             
            /*
 
 
