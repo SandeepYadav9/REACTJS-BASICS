f5045f3bce560e2620e211bc95f9723c16e9303b
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-07-10 20:57:33 +0530

    Update FRAGMENTS-IN-REACT.js

--- FRAGMENTS-IN-REACT.js
+++ FRAGMENTS-IN-REACT.js
@@ -1,3 +1,17 @@
 /*
 FRAGMENTS-IN-REACT
+
+- Fragments is Rapper components in React
+
+ Fragment rapped list of child compnents 
+ 
+ Whout adding extra node in Real-DOM
+ 
+ <Fragment> 
+   <Child1/>
+   <Child2/>
+   <Child3/>
+   <Child4/>
+ </Fragment>
+ 
 */
