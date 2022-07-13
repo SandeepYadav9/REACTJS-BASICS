2456dca79dc147c4d89b6b5f3d933fc37ab40bd3
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-07-13 15:18:28 +0530

    Update LAZY-LOADING-SUSPENSE.js

--- LAZY-LOADING-SUSPENSE.js
+++ LAZY-LOADING-SUSPENSE.js
@@ -1,3 +1,7 @@
 // LAZY-LOADING-
 - When an Application become more havve Than your webpage load sloly 
   in this case which page hase les havee make it load first than second than thirld ......
+
+
+const Customer = React.lazy(() => import("./Customer.js"));
+const Admin = React.lazy(() => import("./Admin.js"));
