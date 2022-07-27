ed3ad2fc9c14a28ef4b7950499884a47a1b813e3
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-07-27 15:40:54 +0530

    Update BEST-WAY-TO-UPDATE-STATE.js

--- BEST-WAY-TO-UPDATE-STATE.js
+++ BEST-WAY-TO-UPDATE-STATE.js
@@ -1,2 +1,26 @@
 
 
+import React,{useState} from "react";
+const App = () => {
+  const [enteredData, setEnteredData] = useState({
+    enteredTitle: "",   
+  });
+  
+   const onChangeTitleHandler = (e) => {
+    setEnteredData((privData) => { 
+      let updateData = { ...privData, enteredTitle: e.target.value };
+      return updateData;
+    });
+  };
+  return (
+    <div>
+     <input
+            type="text"
+            value={enteredData.enteredTitle}
+            onChange={onChangeTitleHandler}
+          />
+    </div>
+  );
+};
+
+export default App;
