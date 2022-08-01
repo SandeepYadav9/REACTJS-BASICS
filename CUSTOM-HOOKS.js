901544eaf18a7b3ffff9099bb5f25c77c5d080ca
Sandeep Yadav <81950280+SandeepYadav9@users.noreply.github.com>
2022-08-01 11:14:01 +0530

    Update CUSTOM-HOOKS.js

--- CUSTOM-HOOKS.js
+++ CUSTOM-HOOKS.js
@@ -1,1 +1,10 @@
-// CUSTOM-HOOKS
+/* CUSTOM-HOOKS
+CUSTOM HOOKS - are allow us to have cleaner functional components,
+               remove logic from UI layer,
+               and parent code duplication by using common
+               useCase to reusable hooks 
+
+
+
+*/
+
